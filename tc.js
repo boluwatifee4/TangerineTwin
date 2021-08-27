5f8545dc0f5852804872996b7df3212541e1bd17
Olaonipekun Boluwatife <boluwatifee4@gmail.com>
2021-08-27 19:46:53 +0100

    animation1

---
+++ tc.js
@@ -0,0 +1,72 @@
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.reveal');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight/7;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+    } else{
+        content.classList.remove('active');
+    }
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.reveal1');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if(contentPosition < screenPosition){
+        content.classList.add('active1');
+    } else{
+        content.classList.remove('active1');
+    }
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.reveal2');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if(contentPosition < screenPosition){
+        content.classList.add('active2');
+    } else{
+        content.classList.remove('active2');
+    }
+});
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.reveal3');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if(contentPosition < screenPosition){
+        content.classList.add('active3');
+    } else{
+        content.classList.remove('active3');
+    }
+});
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.reveal4');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if(contentPosition < screenPosition){
+        content.classList.add('active4');
+    } else{
+        content.classList.remove('active4');
+    }
+});
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.reveal5');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if(contentPosition < screenPosition){
+        content.classList.add('active5');
+    } else{
+        content.classList.remove('active5');
+    }
+});
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.reveal6');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if(contentPosition < screenPosition){
+        content.classList.add('active6');
+    } else{
+        content.classList.remove('active6');
+    }
+});
