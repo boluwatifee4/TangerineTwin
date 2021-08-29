97077895220e7bc57779c0073750de7750966611
Olaonipekun Boluwatife <boluwatifee4@gmail.com>
2021-08-29 01:34:59 +0100

    toggle

--- tc.js
+++ tc.js
@@ -1,5 +1,20 @@
 
+const mainMenu = document.querySelector('.mainMenu');
+const openMenu = document.querySelector('.openMenu');
+const closeMenu = document.querySelector('.closeMenu');
 
+openMenu.addEventListener('click' ,show);
+ closeMenu.addEventListener('click' ,close);
+
+ 
+ function show() {
+     mainMenu.style.display = 'flex';
+     mainMenu.style.display.top = '0';
+ }
+
+ function close() {
+     mainMenu.style.top = '-100010%';
+ }
 
 window.addEventListener('scroll',()=>{
     let content = document.querySelector('.reveal');
